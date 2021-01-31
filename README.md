0441cd1a0f8be339655375e4c99e3ddb9ffb70f3
Chernobylll <78006113+Chernobylll@users.noreply.github.com>
2021-01-31 04:17:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,38 +1,79 @@
 # Huawei-unlock-bootloader
-Huawei-unlock-bootloader
-add model 
 
+## Bootloader Unlocker
+
+After closing the official EMUI website,
+which allowed you to retrieve the code to unlock the bootloader of Huawei/Honor phones, 
+here is a python script to test many possible codes.
+
+It uses a bruteforce method, based on the ( IMEI )identifier to generate unlocking codes.
+
+This will most likely only work on European versions, 
+because these only use numbers in the bootloader unlock code.
+
+I've only had the opportunity to test it on European versions:
+
+Huawei-unlock-bootloader add model
 p20
-
 p30
-
 p30 pro
-
 p20 pro
-
-Honor 5x 
-
-Honor 8x 
-
+Honor 5x
+Honor 8x
 Honor 9x
-
-Honor 10 
-Honor 20
-
+Honor 10 Honor 20
 Honor 10 lite
-
 Huawei p20 lite
-
 Huawei Y6 2019
-
 from fastboot
-
 OneClick
-
 all data will be erased on unlock
-
- imel number
- 
- python script
-
+imel number
+python script
 by Chernobylll
+
+
+## Instructions
+
+1. Enable developer options in Android.
+1. Enable USB debugging in Android.
+1. Connect your device to the computer and launch the script.
+1. Auto batch
+
+    Windows:
+    ```batch
+    C:\WINDOWS\system32> python unlock.py
+    ```
+    Linux:
+    ```shell
+    $ python3 unlock.py
+    ```
+    The device is going to ask for authorisation, which you'll have to allow.  
+    Please also check the checkbox "Always allow from this computer".
+	
+1. Wait for the application to detect your device Huawei/Honor. The device info should appear.
+1. Enter the IMEI (ONLY First Number)
+1. Start the bruteforce.  
+
+    (this may take several ~~2h~~ days) 
+	
+1. If the correct code is found, your phone will either be instantly unlocked or prompt you with an additional confirmation dialogue on the target device.  
+    (all data will be erased on unlock!)
+
+
+## FAQ & Troubleshooting
+
+**- The application doesn't work. Is there anything I should have installed?**  
+Yes, it was developed in python so it needs it to run, version 3. You can install the latest version from [here](https://www.python.org/downloads/) or by using Windows Store.
+
+**- The app on Windows doesn't detect my device even though it's connected and USB debugging is enabled. What could be the issue?**  
+Windows most likely doesn't recognise your device in ADB mode. Install the universal ADB drivers from [here](http://dl.adbdriver.com/upload/adbdriver.zip), reboot your PC and try again.
+
+**- My phone reboots every 5 failed attempts**  
+We are aware of this and have implemented an automatic intentional reboot after 4 attempts.  
+You can manually enable this feature in the code.  
+The script will also automatically detect this behaviour. For this to work reliably, please check the checkbox "Always allow from this computer" in the adb dialogue.
+
+**- The script displays "command invalid"**  
+Huawei has removed the unlock command in EMUI 10,or higher.  
+Downgrade your software and try again. if you get Error.
---
+++ unlock.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+https://github.com/Chernobylll/Huawei-unlock-bootloader
+"""
+
+import time
+#from flashbootlib import test
+import os
+import subprocess
+import math
+
+staticimei = 0          #enter your imei here if you dont want to be asked every start
+quickstart = False      #set to True to not need to confirm on script start, should be used in combination with staticimei
+
+def bruteforceBootloader(increment):
+
+#    algoOEMcode = 0000000000000000
+    algoOEMcode     = 1000000000000000  #base to start bruteforce from
+    autoreboot      = False             #set this to True if you need to prevent the automatic reboot to system by the bootloader after x failed attempts, code will automatically set this to true if it detects a reboot by the bootloader
+    autorebootcount = 4                 #reboot every x attemps if autoreboot is True, set this one below the automatic reboot by the bootloader
+    savecount       = 200               #save progress every 200 attempts, do not set too low to prevent storage wearout
+    unknownfail     = True              #fail if output is unknown, only switch to False if you have problems with this
+    
+    failmsg = "check password failed"   #used to check if code is wrong
+    
+    unlock=False
+    n=0
+    while (unlock == False):
+        print("Bruteforce is running...\nCurrently testing code "+str(algoOEMcode).zfill(16)+"\nProgress: "+str(round((algoOEMcode/10000000000000000)*100, 2))+"%")
+        output = subprocess.run("fastboot oem unlock " + str(algoOEMcode).zfill(16), shell=True, stderr=subprocess.PIPE).stderr.decode('utf-8')
+        print(output)
+        output = output.lower()
+        n+=1
+
+        if 'success' in output:
+            bak = open("unlock_code.txt", "w")
+            bak.write("Your saved bootloader code : "+str(algoOEMcode))
+            bak.close()
+            print("Your bruteforce result has been saved in \"unlock_code.txt\"")
+            return(algoOEMcode)
+        if 'reboot' in output:
+            print("Target device has bruteforce protection!")
+            print("Waiting for reboot and trying again...")
+            os.system("adb wait-for-device")
+            os.system("adb reboot bootloader")
+            print("Device reboot requested, turning on reboot workaround.")
+            autoreboot = True
+        if failmsg in output:
+            #print("Code " + str(algoOEMcode) + " is wrong, trying next one...")
+            pass
+        if 'success' not in output and 'reboot' not in output and failmsg not in output and unknownfail:
+            # fail here to prevent continuing bruteforce on success or another error the script cant handle
+            print("Could not parse output.")
+            print("Please check the output above yourself.")
+            print("If you want to disable this feature, switch variable unknownfail to False")
+            exit()
+
+        if (n%savecount==0):
+            bak = open("unlock_code.txt", "w")
+            bak.write("If you need to pick up where you left off,\nchange the algoOEMcode variable with #base comment to the following value :\n"+str(algoOEMcode))
+            bak.close()
+            print("Your bruteforce progress has been saved in \"unlock_code.txt\"")
+
+        if (n%autorebootcount==0 and autoreboot):
+            print("Rebooting to prevent bootloader from rebooting...")
+            os.system('fastboot reboot bootloader')
+
+        algoOEMcode += increment
+
+        if (algoOEMcode > 10000000000000000):
+            print("OEM Code not found!\n")
+            os.system("fastboot reboot")
+            exit()
+
+def luhn_checksum(imei):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(imei)
+    oddDigits = digits[-1::-2]
+    evenDigits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(oddDigits)
+    for i in evenDigits:
+        checksum += sum(digits_of(i*2))
+    return checksum % 10
+
+
+# Bruteforce setup:
+
+print('\n\n           Unlock Bootloader script - By Chernobylll_\' and programminghoch10')
+print('\n\n  (You must enable USB DEBUGGING and OEM UNLOCK in the developer options of the target device...)')
+print('  !!! All data will be erased !!! \n')
+#input(' Press enter to detect device..\n')
+
+os.system('adb devices')
+
+print("Please select \"Always allow from this computer\" in the adb dialog!")
+
+checksum = 1
+while (checksum != 0):
+    if staticimei == 0: 
+        imei = int(input('Type IMEI: '))
+    if staticimei > 0:
+        imei = staticimei
+    checksum = luhn_checksum(imei)
+    if (checksum != 0):
+        print('IMEI incorrect!')
+        if(staticimei > 0):
+            exit()
+increment = int(math.sqrt(imei)*1024)
+if quickstart==False:
+    input('Press enter to reboot your device...\n')
+os.system('adb reboot bootloader')
+#input('Press enter when your device is ready... (This may take time, depending on your phone)\n')
+
+codeOEM = bruteforceBootloader(increment)
+
+os.system('fastboot getvar unlocked')
+#os.system('fastboot reboot')
+
+print('\n\nDevice unlocked! OEM CODE: '+codeOEM+'\n')
+exit()
